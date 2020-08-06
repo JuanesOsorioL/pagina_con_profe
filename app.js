291663e9ebb8c3da7851531532116b84817ed8e8
juanesosorio <juanchis_1989@hotmail.com>
2020-08-06 13:08:43 -0500

    menu con toggle en js

---
+++ app.js
@@ -0,0 +1,58 @@
+
+/*
+///antes///
+
+function $( selector ) {  
+    const elements = document.querySelectorAll( selector );
+    return ( elements.length > 1 ) ? 
+            elements : 
+            document.querySelector(selector);
+        }
+
+
+const barra =$('i.fa-bars');
+const cerrar =$('i.fa-close');
+const menu =$('div.menu');
+
+
+const activo = function(){
+    barra.classList.add('ocultar');
+    cerrar.classList.remove('ocultar');
+    menu.classList.remove('ocultar');
+}
+const desactivo = function(){
+    barra.classList.remove('ocultar');
+    cerrar.classList.add('ocultar');
+    menu.classList.add('ocultar');
+}
+barra.addEventListener('click', function(){
+    activo();
+})
+cerrar.addEventListener('click', function(){
+    desactivo();
+})
+*/
+
+function menu (){
+    document.getElementById("menu").classList.toggle("ocultar");
+    document.getElementById("menu_open").classList.toggle("ocultar");
+    document.getElementById("menu_close").classList.toggle("ocultar");
+}
+
+document.getElementById("menu_open").onclick=function(){
+    menu();
+}
+document.getElementById("menu_close").onclick=function(){
+    menu();
+}
+
+
+
+
+
+
+
+
+
+
+
